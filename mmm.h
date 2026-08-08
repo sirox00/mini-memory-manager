afa9c8138c6668b1f874f3602e61c160ff50c7fa
sirox00 <vlfdirytuf@gmail.com>
2026-08-08 23:30:32 +0300

    fixing of the api draft

--- mmm.h
+++ mmm.h
@@ -22,8 +22,7 @@ typedef struct mmm_memblock_s {
 } mmm_memblock;
 
 typedef struct {
-    mmm_memblock *free_blocks;
-    mmm_memblock *used_blocks;
+    mmm_memblock *first_block;
 
     uint64_t size;
     uint64_t alignment;
@@ -31,18 +30,20 @@ typedef struct {
     void *mem;
 } mmm_pool;
 
-mmm_pool mmm_pool_init(uint64_t size, uint64_t alignment, uint64_t managed_ptrs_per_block);
+mmm_pool mmm_pool_init(uint64_t size, uint64_t alignment, uint64_t managed_ptrs_per_block, mmm_allocation_callbacks allocation_callbacks);
 mmm_pool mmm_pool_init_preallocated(uint64_t size, uint64_t alignment, uint64_t managed_ptrs_per_block, void *mem);
-void mmm_pool_grow(mmm_pool pool, uint64_t new_size);
-void mmm_pool_purge(mmm_pool pool, uint32_t (*decider_func)(mmm_memblock *p_block));
-void mmm_pool_uninit(mmm_pool pool);
+void mmm_pool_grow(mmm_pool *pool, uint64_t new_size, mmm_allocation_callbacks allocation_callbacks);
+void mmm_pool_grow_preallocated(mmm_pool *pool, uint64_t new_size, void *new_mem);
+void mmm_pool_purge(mmm_pool *pool, uint32_t (*decider_func)(mmm_memblock *p_block, void *p_user_data), void *p_user_data);
+// you dont need to uninit the pool, if it was used only with _preallocated versions of mmm_pool_init and mmm_pool_grow
+void mmm_pool_uninit(mmm_pool *pool, mmm_allocation_callbacks allocation_callbacks);
 
-void *mmm_pool_alloc(mmm_pool pool, uint64_t size, uint64_t tag);
-void *mmm_pool_realloc(mmm_pool pool, void *allocation, uint64_t new_size);
-void mmm_pool_free(mmm_pool pool, void *allocation);
+void *mmm_pool_alloc(mmm_pool *pool, uint64_t size, uint64_t tag);
+void *mmm_pool_realloc(mmm_pool *pool, void *allocation, uint64_t new_size);
+void mmm_pool_free(mmm_pool *pool, void *allocation);
 
 void mmm_set_tag(void *allocation, uint64_t new_tag);
-void mmm_register_managed_ptr(void *allocation, void **p_ptr);
+void mmm_add_managed_ptr(void *allocation, void **p_ptr);
 
 inline mmm_memblock *mmm_ptr_to_block(void *ptr) { return (mmm_memblock *)(ptr - sizeof(mmm_memblock)); }
 inline void *mmm_block_to_ptr(mmm_memblock *p_block) { return (void *)(p_block) + sizeof(mmm_memblock); }
